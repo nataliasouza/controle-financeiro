f19ed60fdefe7f1f8ae65bced6dbc48c25544edb
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-12-06 17:27:49 -0300

    TiposController

---
+++ ControleFinanceio.API/ControleFinanceio.API/Controllers/TiposController.cs
@@ -0,0 +1,27 @@
+﻿using ControleFinanceiro.BLL.Models;
+using ControleFinanceiro.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ControleFinanceio.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TiposController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public TiposController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tipo>>> GetTipos()
+        {
+            return await _context.Tipos.ToListAsync();
+        }
+    }
+}
