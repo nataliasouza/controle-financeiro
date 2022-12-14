84cf722b9a9ec05621cf2bf340cedaf061d5785b
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-12-14 12:04:23 -0300

    Inclui o método VoltarListagem

--- ControleFinanceiro-UI/src/app/components/Categoria/nova-categoria/nova-categoria.component.ts
+++ ControleFinanceiro-UI/src/app/components/Categoria/nova-categoria/nova-categoria.component.ts
@@ -44,4 +44,8 @@ export class NovaCategoriaComponent implements OnInit {
       this.router.navigate(['categorias/listagemcategorias']);
     });
   }
+
+  VoltarListagem() : void {
+    this.router.navigate(['categorias/listagemcategorias']);
+  }
 }
