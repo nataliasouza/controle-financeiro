bd012963697ce64b75f2524884a40f67c494884a
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-12-14 09:59:05 -0300

    Componente atualizar-categoria

--- ControleFinanceiro-UI/src/app/app-routing.module.ts
+++ ControleFinanceiro-UI/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListagemCategoriasComponent } from './components/Categoria/listagem-categorias/listagem-categorias.component';
 import { NovaCategoriaComponent } from './components/Categoria/nova-categoria/nova-categoria.component';
+import { AtualizarCategoriaComponent } from './components/Categoria/atualizar-categoria/atualizar-categoria.component';
 
 const routes: Routes = [
   {
@@ -9,7 +10,11 @@ const routes: Routes = [
   },
   {
     path: 'categorias/novacategoria', component: NovaCategoriaComponent
-  }
+  },
+  {
+    path: 'categorias/atualizarcategoria', component: AtualizarCategoriaComponent
+  },
+
 ];
 
 @NgModule({
--- ControleFinanceiro-UI/src/app/app.module.ts
+++ ControleFinanceiro-UI/src/app/app.module.ts
@@ -22,12 +22,15 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatSelectModule } from '@angular/material/select';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { AtualizarCategoriaComponent } from './components/Categoria/atualizar-categoria/atualizar-categoria.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListagemCategoriasComponent,
-    NovaCategoriaComponent
+    NovaCategoriaComponent,
+    AtualizarCategoriaComponent
   ],
   imports: [
     BrowserModule,
@@ -42,6 +45,7 @@ import { MatSelectModule } from '@angular/material/select';
     MatInputModule,
     MatDividerModule,
     MatSelectModule,
+    MatGridListModule,
     ReactiveFormsModule
   ],
   providers: [
